b735b9d5e71b5be33fe0951eb7e5b66c6cf45e74
emileghattas <emile.ghatas05@gmail.com>
2025-03-18 18:26:48 -0400

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//-----------------------------------------------------------------
+// Assignment 2
+// Question:
+// Written by: Emile Ghattas (id: 40282552)
+//-----------------------------------------------------------------
+
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
